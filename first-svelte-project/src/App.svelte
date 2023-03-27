33bca226dc2d18d06077a27eb6b65c1de3ef4284
breelef <emil_vinther@hotmail.com>
2023-03-27 11:12:40 +0200

    Svelte

---
+++ first-svelte-project/src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Greeter from "./lib/Greeter.svelte";
+    import FavouriteBirds from "./lib/FavouriteBirds.svelte";
+    import Garden from "./lib/Garden.svelte";
+    import GardenGate from "./lib/GardenGate.svelte";
+</script>
+<Greeter name="Hans"/>
+<Greeter name="Morten"/>
+
+<FavouriteBirds/>
+<Garden/>
+<GardenGate/>
---
+++ first-svelte-project/src/lib/FavouriteBirds.svelte
@@ -0,0 +1,23 @@
+<script>
+    import Birds from "./Birds.svelte";
+    const birds = ["Hawk", "Crow"];
+    const birdEmoji = ["🦅", "🐦"]
+
+    let birdIndex = 0;
+    
+    
+    function changeBird(){
+        birdIndex = (birdIndex + 1) % birds.length;
+    }
+</script>
+<div on:click={changeBird} on:keydown={changeBird}>
+    <Birds
+    name={birds[birdIndex]} 
+    emoji={birdEmoji[birdIndex]}
+/>
+</div>
+{#each birds as bird }
+    <Birds name={bird}/>
+{/each}
+
+
